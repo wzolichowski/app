af981b3fba0d1c0185be1717c9f8afc180176589
wzolichowski <wzolichowski@gmail.com>
2025-05-23 10:19:42 +0200

    modified main.tf all working

--- main.tf
+++ main.tf
@@ -1,35 +1,34 @@
 provider "azurerm" {
-    features {}
+  features {}
+  subscription_id = "ccc3b844-dd78-42fe-b922-2be22fd5f654"
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "hello-cloud-rg"
-    location = "west europe"
-
+  name     = "hello-cloud-rg"
+  location = "westeurope"
 }
 
-resource "azurerm_app_service_plan" "plan" {
-    name = "hello-cloud-plan"
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.name
-    sku {
-        tier = "Basic"
-        size = "B1"
-    }
+resource "azurerm_service_plan" "plan" {
+  name                = "hello-cloud-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "app" {
-    name = "hello-cloud-app"
-    location = azurerm_resource_group.location
-    resource_group_name = azurerm_resource_group.name
-    app_service_plan_id = azurerm_app_service_plan.plan.id
+resource "azurerm_linux_web_app" "app" {
+  name                = "hello-cloud-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
 
-    site_config {
-        linux_fx_version ="DOCKER|<username>/<imagename>:latest"
-
+  site_config {
+    application_stack {
+      docker_image_name = "wzolich/app:latest"
     }
+  }
 
-    app_settings = {
-        WEBSITES_PORT = "80"
-    }
+  app_settings = {
+    WEBSITES_PORT = "80"
+  }
 }
